e55951d53e666f32f0de7b0f7e3e118093981b1a
DanielGarate <115984886+DanielGarate@users.noreply.github.com>
2022-11-21 11:54:07 +0100

    Movimiento de dos personajes

--- juego/src/js/main.js
+++ juego/src/js/main.js
@@ -24,7 +24,7 @@ var config = {     //Contiene un json
 
 var game = new Phaser.Game(config);
 
-var player, muros;
+var player1, player2, muros;
 
 
 function preload() {
@@ -55,89 +55,165 @@ function create() {
     muros.create(550, 408, "muro").setScale(0.7, 0.25).refreshBody();  //refresh body es necesario ya que se ha escalado un cuerpo estático
     muros.create(550, 208, "muro").setScale(0.7, 0.25).refreshBody();
     muros.create(650, 308, "muro").setScale(0.7, 0.25).refreshBody();
-    
+
 
 
     //Personaje
-    player = this.physics.add.sprite(100, 450, 'dude'); //Fisica dinamica (dinamic group) por defecto
+    player1 = this.physics.add.sprite(100, 450, 'dude'); //Fisica dinamica (dinamic group) por defecto
+    player2 = this.physics.add.sprite(500, 450, 'dude'); //Fisica dinamica (dinamic group) por defecto
+
     //Posicion del personaje
 
-    player.setCollideWorldBounds(true);
+    player1.setCollideWorldBounds(true);
+    player2.setCollideWorldBounds(true);
 
-    this.anims.create({
+    //Jugador 1
+    player1.anims.create({
         key: 'left',
         frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }), //Usa los fottogramas 0, 1, 2 y 3
         frameRate: 10,
         repeat: -1  //La animación debe volver a empezar cuando termine
     });
 
-    this.anims.create({
+    player1.anims.create({
         key: 'turn',
         frames: [{ key: 'dude', frame: 4 }],
         frameRate: 20
     });
 
-    this.anims.create({
+    player1.anims.create({
         key: 'right',
         frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
         frameRate: 10,
         repeat: -1
     });
 
-    cursors = this.input.keyboard.createCursorKeys();  //Crea el objeto cursors (con 4 propiedades: up, down, left, right)
+    //Jugador 2
+    player2.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }), //Usa los fottogramas 0, 1, 2 y 3
+        frameRate: 10,
+        repeat: -1  //La animación debe volver a empezar cuando termine
+    });
 
-    this.physics.add.collider(player, muros);
+    player2.anims.create({
+        key: 'turn',
+        frames: [{ key: 'dude', frame: 4 }],
+        frameRate: 20
+    });
 
+    player2.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.physics.add.collider(player1, muros);
+    this.physics.add.collider(player2, muros);
+
+    cursors = this.input.keyboard.createCursorKeys();
+    keys = this.input.keyboard.addKeys('A,W,S,D');
 
 }
 
+
+
+
+
 function update(time, delta) //Delta se usa para que en todos los navegadores el movimiento sea el mismo
 {
-const cursors = this.input.keyboard.createCursorKeys();
-
-    
+    //Jugador 1
     if (cursors.down.isDown && cursors.left.isDown) {
-    player.setVelocityY(250);
-    player.setVelocityX(-250);
-    player.anims.play('left', true);
+        player1.setVelocityY(250);
+        player1.setVelocityX(-250);
+        player1.anims.play('left', true);
 
-    }else if (cursors.down.isDown && cursors.right.isDown) {
-        player.setVelocityY(250);
-        player.setVelocityX(250);
-        player.anims.play('left', true);
+    } else if (cursors.down.isDown && cursors.right.isDown) {
+        player1.setVelocityY(250);
+        player1.setVelocityX(250);
+        player1.anims.play('left', true);
 
-    }else if (cursors.up.isDown && cursors.left.isDown) {
-        player.setVelocityY(-250);
-        player.setVelocityX(-250);
-        player.anims.play('left', true);
+    } else if (cursors.up.isDown && cursors.left.isDown) {
+        player1.setVelocityY(-250);
+        player1.setVelocityX(-250);
+        player1.anims.play('left', true);
 
-    }else if (cursors.up.isDown && cursors.right.isDown) {
-        player.setVelocityY(-250);
-        player.setVelocityX(250);
-        player.anims.play('left', true);
+    } else if (cursors.up.isDown && cursors.right.isDown) {
+        player1.setVelocityY(-250);
+        player1.setVelocityX(250);
+        player1.anims.play('left', true);
 
     } else if (cursors.left.isDown) {
-        player.setVelocityX(-250);
+        player1.setVelocityX(-250);
 
-        player.anims.play('left', true);
+        player1.anims.play('left', true);
     }
     else if (cursors.right.isDown) {
-        player.setVelocityX(250);
+        player1.setVelocityX(250);
 
-        player.anims.play('right', true);
+        player1.anims.play('right', true);
 
     } else if (cursors.up.isDown) {
-        player.setVelocityY(-250);
-        player.anims.play('right', true);
+        player1.setVelocityY(-250);
+        player1.anims.play('right', true);
 
     } else if (cursors.down.isDown) {
-        player.setVelocityY(250);
-        player.anims.play('left', true);
-    }else {
-        player.setVelocityX(0);
-        player.setVelocityY(0);
+        player1.setVelocityY(250);
+        player1.anims.play('left', true);
+    } else {
+        player1.setVelocityX(0);
+        player1.setVelocityY(0);
 
-        player.anims.play('turn');
+        player1.anims.play('turn');
     }
+
+
+    //Jugador 2
+    if (keys.S.isDown && keys.A.isDown) {
+        player2.setVelocityY(250);
+        player2.setVelocityX(-250);
+        player2.anims.play('left', true);
+    } else if (keys.S.isDown && keys.D.isDown) {
+        player2.setVelocityY(250);
+        player2.setVelocityX(250);
+        player2.anims.play('left', true);
+
+    } else if (keys.W.isDown && keys.A.isDown) {
+        player2.setVelocityY(-250);
+        player2.setVelocityX(-250);
+        player2.anims.play('left', true);
+
+    } else if (keys.W.isDown && keys.D.isDown) {
+        player2.setVelocityY(-250);
+        player2.setVelocityX(250);
+        player2.anims.play('left', true);
+
+    } else if (keys.A.isDown) {
+        player2.setVelocityX(-250);
+
+        player2.anims.play('left', true);
+    }
+    else if (keys.D.isDown) {
+        player2.setVelocityX(250);
+
+        player2.anims.play('right', true);
+
+    } else if (keys.W.isDown) {
+        player2.setVelocityY(-250);
+        player2.anims.play('right', true);
+
+    } else if (keys.S.isDown) {
+        player2.setVelocityY(250);
+        player2.anims.play('left', true);
+    } else {
+        player2.setVelocityX(0);
+        player2.setVelocityY(0);
+
+        player2.anims.play('turn');
+    }
+
+
+
 
 }
