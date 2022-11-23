c53fcdec9b6df8268580622d19c66c03c97300dc
DanielGarate <115984886+DanielGarate@users.noreply.github.com>
2022-11-23 19:02:16 +0100

    Implementación del game.js subido ayer

---
+++ PracJeR-main/PracJeR-Proyecto/src/js/index.js
@@ -0,0 +1,20 @@
+import { Game } from './scenes/game.js';
+
+
+var config = {     //Contiene un json
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  parent: "container",
+  type: Phaser.AUTO,
+  scene: [Game],
+  physics: {    //Se agrega el sistema de fisicas arcade a la configuración
+      default: 'arcade',
+      arcade: {
+          gravity: { y: 0 },
+          debug: false
+      }
+  }
+};
+
+var game = new Phaser.Game(config);
