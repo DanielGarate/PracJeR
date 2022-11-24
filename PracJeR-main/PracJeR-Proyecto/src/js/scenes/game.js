4103c8e5bb914b3f4a99431efdb9c2dd28e58c22
alexcav37 <alejandrocavero371@gmail.com>
2022-11-24 19:02:06 +0100

    arreglado el contador y formato
    mejorado el formato

--- PracJeR-main/PracJeR-Proyecto/src/js/scenes/game.js
+++ PracJeR-main/PracJeR-Proyecto/src/js/scenes/game.js
@@ -4,274 +4,267 @@
 //INTENTO DE MARCADOR
 var score1 = 0;
 var score2 = 0;
-var scoreBoard1,scoreBoard2;
-var player1, player2, muros, limites, bombas,x,y, t1, t2, textoFinPartida; //se sacan las variables fuera de la clase
+var scoreBoard1, scoreBoard2;
+var player1, player2, muros, limites, bombas, x, y, t1, t2, textoFinPartida; //se sacan las variables fuera de la clase
 var t1 = 1000;
 var t2 = 1000;
 //intento clase jugador 
-class Jugador{
-  constructor(vida){
-    this.vida=vida;
-  }
-  get getVida(){
-    return this.vida;
-  }
-  set setVida(value){
-    this.vida = value;
-  }
+class Jugador {
+    constructor(vida) {
+        this.vida = vida;
+    }
+    get getVida() {
+        return this.vida;
+    }
+    set setVida(value) {
+        this.vida = value;
+    }
 }
 export class Game extends Phaser.Scene {
-  constructor() {
-    super({ key: 'game' });
-  }
-  preload() {
-    this.load.image("campo", "../../resources/img/campo.png");
-    this.load.spritesheet("dude",
-        "../../resources/img/dude.png",
-        { frameWidth: 32, frameHeight: 48 }
-    );
-
-    this.load.image("muro", "../../resources/img/muro.png");
-    this.load.image("separacion", "../../resources/img/separacion.png");
-    this.load.image("bomba", "../../resources/img/bomb.png");
-  }
-
-  create() {
-   
-    this.add.image(400, 300, "campo");  //Añade un nuevo elemento del juego 
-    //de tipo imagen a la lista de objetos de la escena.
-    this.add.image(400, 300, "separacion");  
-
-    //Muros
-    muros = this.physics.add.staticGroup();
-    
-    limites = this.physics.add.staticGroup();
-
-    //Muros lado izquierdo
-    muros.create(250, 408, "muro").setScale(0.7, 0.25).refreshBody();  //refresh body es necesario ya que se ha escalado un cuerpo estático
-    muros.create(250, 208, "muro").setScale(0.7, 0.25).refreshBody();
-    muros.create(150, 308, "muro").setScale(0.7, 0.25).refreshBody();
-
-    //Muros lado derecho
-    muros.create(550, 408, "muro").setScale(0.7, 0.25).refreshBody();  //refresh body es necesario ya que se ha escalado un cuerpo estático
-    muros.create(550, 208, "muro").setScale(0.7, 0.25).refreshBody();
-    muros.create(650, 308, "muro").setScale(0.7, 0.25).refreshBody();
-
-    limites.create(400, 300, "separacion");
-
-    //Personaje
-    player1 = this.physics.add.sprite(100, 450, 'dude'); //Fisica dinamica (dinamic group) por defecto
-    player2 = this.physics.add.sprite(500, 450, 'dude'); //Fisica dinamica (dinamic group) por defecto
-
-    //INSTANCIACION JUGADOR 1
-    const playerC1 = new Jugador(10);
-    const playerC2 = new Jugador(10);
-
-    //Posicion del personaje
-    player1.setCollideWorldBounds(true);
-    player2.setCollideWorldBounds(true);
-    //creación de animaciones
-    this.anims.create({
-        key: 'left',
-        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }), //Usa los fottogramas 0, 1, 2 y 3
-        frameRate: 10,
-        repeat: -1  //La animación debe volver a empezar cuando termine
-    });
-
-    this.anims.create({
-        key: 'turn',
-        frames: [{ key: 'dude', frame: 4 }],
-        frameRate: 20
-    });
-
-    this.anims.create({
-        key: 'right',
-        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-        frameRate: 10,
-        repeat: -1
-    });
-    
-    this.cursors = this.input.keyboard.createCursorKeys();  //Crea el objeto cursors (con 4 propiedades: up, down, left, right)
-    this.keys = this.input.keyboard.addKeys('A,W,S,D,E,L');
-
-    this.physics.add.collider(player1, muros);
-    this.physics.add.collider(player2, muros);
-    this.physics.add.collider(player1, limites);
-    this.physics.add.collider(player2, limites);
-
-    bombas = this.physics.add.group();
-    this.physics.add.collider(player1, bombas, hitBomb, null, this);
-   
-    //INTENTO DE MARCADOR 2
-    scoreBoard2 = this.add.text(440, 40, "P2: 0", {fontSize: '32px', fill: '#fff'});
-    scoreBoard1 = this.add.text(240, 40, "P1: 0", {fontSize: '32px', fill: '#fff'});
-
-    //algoritmo que detecta bomba contra jugador detiene la partida
-    //HAY ALGUN PROBLEMA CON LA DETECCION DE JUGADOR UN Y 2 TODO
-    function hitBomb (player1, bomba)
-   {
-    playerC1.setVida = 0;
-    console.log("hola2");
-    marcador();
-    player1.setActive(false);
-    bomba.destroy();
-    this.physics.pause();
-    player1.anims.play('turn');
-
-    textoFinPartida = this.add.text(200, 200, 'Fin de partida \n Jugador 1 gana', 
-    { fontSize: '50px', fill: '#000' });
-   }
-   
-   this.physics.add.collider(muros, bombas, hitBomb1, null, this);
-   
-   function hitBomb1 (muro, bomba)
-   {
-    if(muro.vida < 0){
-    muro.destroy();
-    bomba.destroy();
-    }else{
-    muro.vida--;  
+    constructor() {
+        super({key: 'game'});
     }
-   }
+    preload() {
+        this.load.image("campo", "../../resources/img/campo.png");
+        this.load.spritesheet("dude",
+                "../../resources/img/dude.png",
+                {frameWidth: 32, frameHeight: 48}
+        );
 
-    this.physics.add.collider(player2, bombas, hitBomb, null, this);
-
-   function hitBomb (player2, bomba)
-   {
-    console.log("hola1");
-    playerC2.setVida = 0;
-    marcador();
-    player2.setActive(false);
-    bomba.destroy();
-    this.physics.pause();
-    player2.anims.play('turn');
-    player1.anims.play('turn');
-    textoFinPartida = this.add.text(200, 200, 'Fin de partida \n Jugador 2 gana', 
-    { fontSize: '50px', fill: '#000' });
-   }
-
-    //INTENTO DE MARCADOR 3
-    function marcador(){
-      if(playerC1.getVida == 0){
-        score1 += 1;
-        scoreBoard1.setText('P1: ' + score1);
-        console.log("hola");
-      }
-      else if(playerC2.getVida== 0){
-        score2 += 5;
-        scoreBoard2.setText('P2: ' + score2);
-      }
-      
+        this.load.image("muro", "../../resources/img/muro.png");
+        this.load.image("separacion", "../../resources/img/separacion.png");
+        this.load.image("bomba", "../../resources/img/bomb.png");
     }
-}
+
+    create() {
+
+        this.add.image(400, 300, "campo");  //Añade un nuevo elemento del juego 
+        //de tipo imagen a la lista de objetos de la escena.
+        this.add.image(400, 300, "separacion");
+
+        //Muros
+        muros = this.physics.add.staticGroup();
+
+        limites = this.physics.add.staticGroup();
+
+        //Muros lado izquierdo
+        muros.create(250, 408, "muro").setScale(0.7, 0.25).refreshBody();  //refresh body es necesario ya que se ha escalado un cuerpo estático
+        muros.create(250, 208, "muro").setScale(0.7, 0.25).refreshBody();
+        muros.create(150, 308, "muro").setScale(0.7, 0.25).refreshBody();
+
+        //Muros lado derecho
+        muros.create(550, 408, "muro").setScale(0.7, 0.25).refreshBody();  //refresh body es necesario ya que se ha escalado un cuerpo estático
+        muros.create(550, 208, "muro").setScale(0.7, 0.25).refreshBody();
+        muros.create(650, 308, "muro").setScale(0.7, 0.25).refreshBody();
+
+        limites.create(400, 300, "separacion");
+
+        //Personaje
+        player1 = this.physics.add.sprite(100, 450, 'dude'); //Fisica dinamica (dinamic group) por defecto
+        player2 = this.physics.add.sprite(500, 450, 'dude'); //Fisica dinamica (dinamic group) por defecto
+
+        //INSTANCIACION JUGADOR 1
+        const playerC1 = new Jugador(10);
+        const playerC2 = new Jugador(10);
+
+        //Posicion del personaje
+        player1.setCollideWorldBounds(true);
+        player2.setCollideWorldBounds(true);
+        //creación de animaciones
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('dude', {start: 0, end: 3}), //Usa los fottogramas 0, 1, 2 y 3
+            frameRate: 10,
+            repeat: -1  //La animación debe volver a empezar cuando termine
+        });
+
+        this.anims.create({
+            key: 'turn',
+            frames: [{key: 'dude', frame: 4}],
+            frameRate: 20
+        });
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('dude', {start: 5, end: 8}),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.cursors = this.input.keyboard.createCursorKeys();  //Crea el objeto cursors (con 4 propiedades: up, down, left, right)
+        this.keys = this.input.keyboard.addKeys('A,W,S,D,E,L');
+
+        this.physics.add.collider(player1, muros);
+        this.physics.add.collider(player2, muros);
+        this.physics.add.collider(player1, limites);
+        this.physics.add.collider(player2, limites);
+
+        bombas = this.physics.add.group();
+        this.physics.add.collider(player1, bombas, hitBomb, null, this);
 
 
+        //INTENTO DE MARCADOR 2
+        scoreBoard2 = this.add.text(440, 40, "P2: 0", {fontSize: '32px', fill: '#fff'});
+        scoreBoard1 = this.add.text(240, 40, "P1: 0", {fontSize: '32px', fill: '#fff'});
 
+        //algoritmo que detecta bomba contra jugador detiene la partida
+        //HAY ALGUN PROBLEMA CON LA DETECCION DE JUGADOR UN Y 2 TODO
+        function hitBomb(player1, bomba)
+        {
+            playerC1.setVida = 0;
+            console.log("hola2");
+            marcador();
+            player1.setActive(false);
+            bomba.destroy();
+            this.physics.pause();
+            player1.anims.play('turn');
 
+            textoFinPartida = this.add.text(200, 200, 'Fin de partida \n Jugador 1 gana',
+                    {fontSize: '50px', fill: '#000'});
+        }
 
-  update(time, delta) //Delta se usa para que en todos los navegadores el movimiento sea el mismo
-  {
-  //Jugador 1
-      if (this.cursors.down.isDown && this.cursors.left.isDown) {
-        player1.setVelocityY(250);
-        player1.setVelocityX(-250);
-        player1.anims.play('left', true);
-            
-      }  else if (this.cursors.down.isDown && this.cursors.right.isDown) {
-        player1.setVelocityY(250);
-        player1.setVelocityX(250);
-        player1.anims.play('left', true);
-      } else if (this.cursors.up.isDown && this.cursors.left.isDown) {
-        player1.setVelocityY(-250);
-        player1.setVelocityX(-250);
-        player1.anims.play('left', true);
-      } else if (this.cursors.up.isDown && this.cursors.right.isDown) {
-        player1.setVelocityY(-250);
-          player1.setVelocityX(250);
-          player1.anims.play('left', true);
-      } else if (this.cursors.left.isDown) {
-        player1.setVelocityX(-250);
-        player1.anims.play('left', true);
-      }
-      else if (this.cursors.right.isDown) {
-        player1.setVelocityX(250);
-        player1.anims.play('right', true);
-      } else if (this.cursors.up.isDown) {
-        player1.setVelocityY(-250);
-        player1.anims.play('right', true);
-      } else if (this.cursors.down.isDown) {
-        player1.setVelocityY(250);
-        player1.anims.play('left', true);
-      } else {
-        player1.setVelocityX(0);
-        player1.setVelocityY(0);
-        player1.anims.play('turn');
-      }
-      //Jugador 2
-      if (this.keys.S.isDown && this.keys.A.isDown) {
-        player2.setVelocityY(250);
-        player2.setVelocityX(-250);
-        player2.anims.play('left', true);
-    } else if (this.keys.S.isDown && this.keys.D.isDown) {
-        player2.setVelocityY(250);
-        player2.setVelocityX(250);
-        player2.anims.play('left', true);
+        this.physics.add.collider(muros, bombas, hitBomb1, null, this);
 
-    } else if (this.keys.W.isDown && this.keys.A.isDown) {
-        player2.setVelocityY(-250);
-        player2.setVelocityX(-250);
-        player2.anims.play('left', true);
+        function hitBomb1(muro, bomba)
+        {
+            if (muro.vida < 0) {
+                muro.destroy();
+                bomba.destroy();
+            } else {
+                muro.vida--;
+            }
+        }
 
-    } else if (this.keys.W.isDown && this.keys.D.isDown) {
-        player2.setVelocityY(-250);
-        player2.setVelocityX(250);
-        player2.anims.play('left', true);
+        this.physics.add.collider(player2, bombas, hitBomb2, null, this);
 
-    } else if (this.keys.A.isDown) {
-        player2.setVelocityX(-250);
+        function hitBomb2(player2, bomba)
+        {
+            console.log("hola1");
+            playerC2.setVida = 0;
+            marcador();
+            player2.setActive(false);
+            bomba.destroy();
+            this.physics.pause();
+            player2.anims.play('turn');
+            player1.anims.play('turn');
+            textoFinPartida = this.add.text(200, 200, 'Fin de partida \n Jugador 2 gana',
+                    {fontSize: '50px', fill: '#000'});
+        }
 
-        player2.anims.play('left', true);
+        //INTENTO DE MARCADOR 3
+        function marcador() {
+            if (playerC1.getVida == 0) {
+                score1 += 5;
+                scoreBoard2.setText('P2: ' + score1);
+                console.log("hola");
+            } else if (playerC2.getVida == 0) {
+                score2 += 5;
+                scoreBoard1.setText('P1: ' + score2);
+            }
+        }
     }
-    else if (this.keys.D.isDown) {
-        player2.setVelocityX(250);
 
-        player2.anims.play('right', true);
-
-    } else if (this.keys.W.isDown) {
-        player2.setVelocityY(-250);
-        player2.anims.play('right', true);
-
-    } else if (this.keys.S.isDown) {
-        player2.setVelocityY(250);
-        player2.anims.play('left', true);
-    } else {
-        player2.setVelocityX(0);
-        player2.setVelocityY(0);
-
-        player2.anims.play('turn');
-    }
-    if(this.keys.E.isDown)
+    update(time, delta) //Delta se usa para que en todos los navegadores el movimiento sea el mismo
     {
-      if(t1 < time - 1000){
-      this.bomba=bombas.create(player2.x-2,player2.y,'bomba');
-      this.bomba.setBounce(1);
-      this.bomba.setCollideWorldBounds(true);
-      this.bomba.setVelocity(Phaser.Math.Between(-200, 0), 20);
-      t1 = time;
-      }
-    }
-    if(this.keys.L.isDown)
-    {
-      if(t2 < time - 1000)
-      {
-        this.bomba=bombas.create(player1.x+2,player1.y,'bomba');
-        this.bomba.setBounce(1);
-        this.bomba.setCollideWorldBounds(true);
-        this.bomba.setVelocity(Phaser.Math.Between(0, 200), 20);
-        t2 = time;
-      }
-    }
-    
+        //Jugador 1
+        if (this.cursors.down.isDown && this.cursors.left.isDown) {
+            player1.setVelocityY(250);
+            player1.setVelocityX(-250);
+            player1.anims.play('left', true);
 
-  }
+        } else if (this.cursors.down.isDown && this.cursors.right.isDown) {
+            player1.setVelocityY(250);
+            player1.setVelocityX(250);
+            player1.anims.play('left', true);
+        } else if (this.cursors.up.isDown && this.cursors.left.isDown) {
+            player1.setVelocityY(-250);
+            player1.setVelocityX(-250);
+            player1.anims.play('left', true);
+        } else if (this.cursors.up.isDown && this.cursors.right.isDown) {
+            player1.setVelocityY(-250);
+            player1.setVelocityX(250);
+            player1.anims.play('left', true);
+        } else if (this.cursors.left.isDown) {
+            player1.setVelocityX(-250);
+            player1.anims.play('left', true);
+        } else if (this.cursors.right.isDown) {
+            player1.setVelocityX(250);
+            player1.anims.play('right', true);
+        } else if (this.cursors.up.isDown) {
+            player1.setVelocityY(-250);
+            player1.anims.play('right', true);
+        } else if (this.cursors.down.isDown) {
+            player1.setVelocityY(250);
+            player1.anims.play('left', true);
+        } else {
+            player1.setVelocityX(0);
+            player1.setVelocityY(0);
+            player1.anims.play('turn');
+        }
+        //Jugador 2
+        if (this.keys.S.isDown && this.keys.A.isDown) {
+            player2.setVelocityY(250);
+            player2.setVelocityX(-250);
+            player2.anims.play('left', true);
+        } else if (this.keys.S.isDown && this.keys.D.isDown) {
+            player2.setVelocityY(250);
+            player2.setVelocityX(250);
+            player2.anims.play('left', true);
+
+        } else if (this.keys.W.isDown && this.keys.A.isDown) {
+            player2.setVelocityY(-250);
+            player2.setVelocityX(-250);
+            player2.anims.play('left', true);
+
+        } else if (this.keys.W.isDown && this.keys.D.isDown) {
+            player2.setVelocityY(-250);
+            player2.setVelocityX(250);
+            player2.anims.play('left', true);
+
+        } else if (this.keys.A.isDown) {
+            player2.setVelocityX(-250);
+
+            player2.anims.play('left', true);
+        } else if (this.keys.D.isDown) {
+            player2.setVelocityX(250);
+
+            player2.anims.play('right', true);
+
+        } else if (this.keys.W.isDown) {
+            player2.setVelocityY(-250);
+            player2.anims.play('right', true);
+
+        } else if (this.keys.S.isDown) {
+            player2.setVelocityY(250);
+            player2.anims.play('left', true);
+        } else {
+            player2.setVelocityX(0);
+            player2.setVelocityY(0);
+
+            player2.anims.play('turn');
+        }
+        if (this.keys.E.isDown)
+        {
+            if (t1 < time - 1000) {
+                this.bomba = bombas.create(player2.x - 2, player2.y, 'bomba');
+                this.bomba.setBounce(1);
+                this.bomba.setCollideWorldBounds(true);
+                this.bomba.setVelocity(Phaser.Math.Between(-200, 0), 20);
+                t1 = time;
+            }
+        }
+        if (this.keys.L.isDown)
+        {
+            if (t2 < time - 1000)
+            {
+                this.bomba = bombas.create(player1.x + 2, player1.y, 'bomba');
+                this.bomba.setBounce(1);
+                this.bomba.setCollideWorldBounds(true);
+                this.bomba.setVelocity(Phaser.Math.Between(0, 200), 20);
+                t2 = time;
+            }
+        }
+
+
+    }
 }
