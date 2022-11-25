3d6176ffef87006e33e9fe0609b1b73b5e63e830
DanielGarate <115984886+DanielGarate@users.noreply.github.com>
2022-11-25 19:58:42 +0100

    Prototipo sin menus

--- PracJeR-Proyecto/src/js/index.js
+++ PracJeR-Proyecto/src/js/index.js
@@ -1,13 +1,12 @@
 import { Game } from './scenes/game.js';
-import{menuIni} from './scenes/menuIni.js';
-import{pantallaFin} from './scenes/pantallaFinal.js';
+
 var config = {     //Contiene un json
   type: Phaser.AUTO,
   width: 800,
   height: 600,
   parent: "container",
   type: Phaser.AUTO,
-  scene: [menuIni,Game,pantallaFin],
+  scene: [Game],
   physics: {    //Se agrega el sistema de fisicas arcade a la configuración
       default: 'arcade',
       arcade: {
